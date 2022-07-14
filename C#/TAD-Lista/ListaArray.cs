1877e596798ff6a605bebc2be8da1ba34f8828a1
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-07-14 13:46:39 -0300

    adicionando classe de exceção e metodos

--- C#/TAD-Lista/ListaArray.cs
+++ C#/TAD-Lista/ListaArray.cs
@@ -5,88 +5,159 @@ namespace Lista
         private object[] lista;
 
 
-        public ListaArray(){
+        public ListaArray()
+        {
             lista = new object[1];
         }
 
-        public bool isEmpty(){
-            return list.Length == 0;
+        public bool isEmpty()
+        {
+            return lista.Length == 0;
         }
 
-        public int size(){
-            return list.Length;
+        public int size()
+        {
+            return lista.Length;
         }
 
-        public bool isFirst(object element){
-            if(isEmpty()){
-                //exceção
-            }else{
+        public bool isFirst(object element)
+        {
+            if (isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
                 return element == lista[0];
             }
         }
 
-        public bool isLast(object element){
+        public bool isLast(object element)
+        {
 
-            if(isEmpty()){
-                //exceção
-            }else{
-                return element == lista[size()-1];
+            if (isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                return element == lista[size() - 1];
             }
 
         }
 
-        public object first(){
-            if(isEmpty()){
-                //exceção
-            }else{
+        public object first()
+        {
+            if (isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
                 return lista[1];
             }
         }
 
-        public object last(){
-            if(isEmpty()){
-                //exceção
-            }else{
-                return lista[size()-1];
+        public object last()
+        {
+            if (isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                return lista[size() - 1];
             }
         }
 
-        public object before(int pos){
-            if(pos > size() || pos <= 1 || isEmpty()){
-                //exceção
-            }else{
-                return lista[pos-2];
+        public object before(int pos)
+        {
+            if (pos > size() || pos <= 1 || isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                return lista[pos - 2];
             }
         }
 
-        public object after(int pos){
-             if(pos >= size() || pos < 1 || isEmpty()){
-                 //exceção
-             }else{
-                 return lista[pos];
-             }
+        public object after(int pos)
+        {
+            if (pos >= size() || pos < 1 || isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                return lista[pos];
+            }
         }
 
-        public object elementAtRank(int rank){
-            if(rank >= size() || rank < 0 || isEmpty()){
-                //exceção
-            }else{
+        public object elementAtRank(int rank)
+        {
+            if (rank >= size() || rank < 0 || isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
                 return lista[rank];
-            } 
+            }
         }
 
 
-        //para implementar
-        public object removeAtRank(int rank){
-            if(rank >= size() || rank < 0 || isEmpty()){
-                //exceção
-            }else{
-                
+        public object removeAtRank(int rank)
+        {
+            if (rank >= size() || rank < 0 || isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                object element = lista[rank];
+                for (int i = rank; i < size() - 1; i++)
+                {
+                    lista[i] = lista[i + 1];
+                }
+                lista[size() - 1] = null;
+                return element;
             }
         }
 
 
 
+        public void replaceAtRank(int rank, object element)
+        {
+            if (rank > size() || rank < 0 || isEmpty())
+            {
+                throw new listaException("Lista vazia");
+            }
+            else
+            {
+                if (lista.Length == size())
+                {
+                    increasseArray();
+                }
+                lista[rank] = element;
+            }
+
+
+        }
+
+
+
+
+        private void increasseArray()
+            {
+                object[] newLista = new object[size() * 2];
+                for (int i = 0; i < size(); i++)
+                {
+                    newLista[i] = lista[i];
+                }
+                lista = newLista;
+            }
 
     }
+
 }
---
+++ C#/TAD-Lista/ListaException.cs
@@ -0,0 +1,14 @@
+namespace Lista{
+
+
+    class listaException : Exception
+    {
+        public listaException(string message) : base(message)
+        {
+        }
+    }
+
+
+
+
+}
