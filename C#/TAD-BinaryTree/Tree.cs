a7cb1e709b0217b346591c67f0871f1b2e7e17c2
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-08-09 15:40:34 -0300

    criando loop infinito

--- C#/TAD-BinaryTree/Tree.cs
+++ C#/TAD-BinaryTree/Tree.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 namespace BinaryTree
 {
 
@@ -62,9 +63,9 @@ namespace BinaryTree
             switch (Comparer.Compare(node.Element, element))
             {
                 case -1:
-                    return search(node.Left, element);
-                case 1:
                     return search(node.Right, element);
+                case 1:
+                    return search(node.Left, element);
                 case 0:
                     return node; 
                 default:
@@ -83,19 +84,6 @@ namespace BinaryTree
                 switch (Comparer.Compare(aux.Element, element))
                 {
                     case -1:
-                        if (aux.Left == null)
-                        {
-                            aux.Left = node;
-                            node.Parent = aux;
-                            this.size++;
-                            return;
-                        }
-                        else
-                        {
-                            aux = aux.Left;
-                        }
-                        break;
-                    case 1:
                         if (aux.Right == null)
                         {
                             aux.Right = node;
@@ -106,6 +94,19 @@ namespace BinaryTree
                         else
                         {
                             aux = aux.Right;
+                        }
+                        break;
+                    case 1:
+                        if (aux.Left == null)
+                        {
+                            aux.Left = node;
+                            node.Parent = aux;
+                            this.size++;
+                            return;
+                        }
+                        else
+                        {
+                            aux = aux.Left;
                         }
                         break;
                     case 0:
@@ -124,15 +125,16 @@ namespace BinaryTree
         private TreeNode remove(TreeNode node, object element){ 
             
             TreeNode aux = search(node, element);
+
             //caso não possua filho algum
             if(aux.isExternal()){
                 switch (Comparer.Compare(aux.Element, element))
                 {
                     case -1:
-                        aux.Parent.Left = null;
+                        aux.Parent.Right = null;
                         break;
                     case 1:
-                        aux.Parent.Right = null;
+                        aux.Parent.Left = null;
                         break;
                     case 0:
                         aux.Parent = null;
@@ -156,17 +158,43 @@ namespace BinaryTree
                 return aux;
             //caso possua filho a direita e a esquerda
             }else{
-                TreeNode aux2 = min(aux.Right);
-                aux2.Parent.Left = null;
-                aux2.Left = aux.Left;
-                aux2.Right = aux.Right;
-                aux2.Parent = aux.Parent;
+                TreeNode aux2 = aux.Right;
+                aux2 = min(aux2);
+                // verificar se possui filhos a direita
+                // caso possua, realocar o no pai
+                if(aux2.hasRight()){
+                    aux2.Right.Parent = aux2.Parent;
+                    aux2.Parent.Left = aux2.Right;
+                }
+                //procedimentos de remoção
+                //1 - vincular os filhos do no a a ser removido ao substituto
                 aux.Left.Parent = aux2;
                 aux.Right.Parent = aux2;
-                this.size--;
+                aux2.Left = aux.Left;
+                aux2.Right = aux.Right;
+                //2 - vincular o pai do antigo node ao novo
+                //verifica se o no a ser removido é o root
+                if(isRoot(aux)){
+                    aux2.Parent = null;
+                    this.root = aux2;
+                }else{
+                    aux2.Parent = aux.Parent;
+                    switch (Comparer.Compare(aux.Element, aux.Parent.Element))
+                    {
+                        case -1:
+                            aux.Parent.Right = aux2;
+                            break;
+                        case 1:
+                            aux.Parent.Left = aux2;
+                            break;
+                        default:
+                            break;
+                    }
+                }
                 return aux;
             }
 
+           
         }
 
         //retorna o menor filho
@@ -178,7 +206,24 @@ namespace BinaryTree
             return node;
         }
 
+        //percorre o array em nivel
+        public void level_order(){
+            Queue level = new Queue();
 
+            level.Enqueue(this.root);
+
+            while (level.Count != 0){
+                TreeNode actualNode = (TreeNode)level.Dequeue();
+                Console.WriteLine(actualNode.Element);
+                if(actualNode.hasLeft()){
+                    level.Enqueue(actualNode.Left);
+                }
+                if(actualNode.hasRight()){
+                    level.Enqueue(actualNode.Right);
+                }
+            }
+
+        }
         
     }
 
